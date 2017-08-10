d21262d223a3bd89b8558ad4dbe820be2ed70281
Qu Hao <quhxuxm_quh@gmail.com>
2017-08-10 23:28:27 +0800

    initial mvc

--- entry/src/main/java/com/quhxuxm/quh/proj/tonghao/entry/controller/IndexController.java
+++ entry/src/main/java/com/quhxuxm/quh/proj/tonghao/entry/controller/IndexController.java
@@ -2,11 +2,13 @@ package com.quhxuxm.quh.proj.tonghao.entry.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/index")
+@RequestMapping(path = "/index")
 public class IndexController {
-    @RequestMapping("/hello")
+    @ResponseBody
+    @RequestMapping(path = "/hello")
     public String hello() {
         return "Hello World";
     }
