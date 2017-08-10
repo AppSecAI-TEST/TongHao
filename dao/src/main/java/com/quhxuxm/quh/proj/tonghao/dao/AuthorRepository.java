67a9305e45be40761e196f6283eabf7891221192
Qu Hao <quhxuxm_quh@gmail.com>
2017-08-10 22:20:12 +0800

    Adding domain objects

---
+++ dao/src/main/java/com/quhxuxm/quh/proj/tonghao/dao/AuthorRepository.java
@@ -0,0 +1,7 @@
+package com.quhxuxm.quh.proj.tonghao.dao;
+
+import com.quhxuxm.quh.proj.tonghao.domain.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+}
